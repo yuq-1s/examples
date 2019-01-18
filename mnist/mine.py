728d6515c2024fb33cd573acb8becfbcab4da63e
yuq-1s <zxdewr@sjtu.edu.cn>
2019-01-18 23:17:43 +0800

    Add: evaluation for n-hot

--- mnist/mine.py
+++ mnist/mine.py
@@ -12,6 +12,19 @@ import matplotlib.pyplot as plt
 
 DTYPE = torch.float16
 
+def get_loader(train, args, start_idx, **kwargs):
+    from scattered_sampler import ScatteredSampler
+    dataset = datasets.MNIST('../data', train=train, download=True,
+                    transform=transforms.Compose([
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.1307,), (0.3081,))
+                    ]))
+    dataset = sorted(dataset, key=lambda x: x[1])
+    return torch.utils.data.DataLoader(
+        dataset,
+        sampler=ScatteredSampler(dataset, start_idx=start_idx, order=args.n_hot),
+        batch_size=args.batch_size, **kwargs)
+
 def n_hot(ids, C, n=4):
     """
     ids: (list, ndarray) shape:[batch_size]
@@ -135,7 +148,8 @@ def train(args, model, device, train_loader, optimizer, epoch):
         optimizer.step()
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
+                epoch, batch_idx * len(data),
+                len(train_loader.dataset) // args.n_hot,
                 100. * batch_idx / len(train_loader), loss.item()))
 
 def test(args, model, device, test_loader):
@@ -144,17 +158,26 @@ def test(args, model, device, test_loader):
     correct = 0
     with torch.no_grad():
         for data, target in test_loader:
+            data = data.reshape((-1, 1, 28*args.n_hot, 28))
+            target = target.view(-1, args.n_hot)
             data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
+            output = torch.exp(model(data))
+            output, target = output.to("cpu"), target.to("cpu")
+            test_loss += F.l1_loss(output, n_hot(target, C=10, n=args.n_hot),
+                                   reduction='sum').item() # sum up batch loss
+            _, pred = output.topk(args.n_hot, dim=1)
+            pred, _ = pred.sort(dim=1)
+            target, _ = target.sort(dim=1)
+            # pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).all(dim=1).sum().item()
+            # import ipdb
+            # ipdb.set_trace()
 
     test_loss /= len(test_loader.dataset)
 
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+        test_loss, correct, len(test_loader.dataset) // args.n_hot,
+        100. * correct / (len(test_loader.dataset) // args.n_hot)))
 
 def main():
     # Training settings
@@ -184,28 +207,21 @@ def main():
 
     torch.manual_seed(args.seed)
 
-    device = torch.device("cuda")
+    device = torch.device("cuda" if use_cuda else "cpu")
 
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
-    START_IDX = [0, 5923, 12665, 18623, 24754, 30596, 36017, 41935, 48200, 54051]
-    from scattered_sampler import ScatteredSampler
-    dataset = datasets.MNIST('../data', train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ]))
-    dataset = sorted(dataset, key=lambda x: x[1])
-    train_loader = torch.utils.data.DataLoader(
-        dataset,
-        sampler=ScatteredSampler(dataset, start_idx=START_IDX, order=args.n_hot),
-        batch_size=args.batch_size, **kwargs)
-    # test_loader = ScatteredLoader(
-        # datasets.MNIST('../data', train=False, transform=transforms.Compose([
-                           # transforms.ToTensor(),
-                           # transforms.Normalize((0.1307,), (0.3081,))
-                       # ])),
-        # batch_size=args.test_batch_size, shuffle=True, **kwargs)
-
+    start_idx = [0, 5923, 12665, 18623, 24754, 30596, 36017, 41935, 48200, 54051]
+    train_loader = get_loader(
+        train=True, args=args,
+        start_idx=[0, 5923, 12665, 18623, 24754, 30596, 36017, 41935, 48200,
+                   54051],
+        **kwargs
+    )
+    test_loader = get_loader(
+        train=False, args=args,
+        start_idx=[0, 980, 2115, 3147, 4157, 5139, 6031, 6989, 8017, 8991],
+        **kwargs
+    )
 
     # model = Net().to(device)
     model = ResNet(block=ResidualBlock, layers=[2, 2, 3, 3]).to(device)
@@ -213,10 +229,10 @@ def main():
 
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
-        # test(args, model, device, test_loader)
+        test(args, model, device, test_loader)
 
     if (args.save_model):
         torch.save(model.state_dict(),"mnist_cnn.pt")
-        
+
 if __name__ == '__main__':
     main()
